8a7c9c846348a972094e158c2db02c264d133f01
Fl1ckerxD <mihaylav.slava@yandex.ru>
2025-01-22 00:19:24 +0300

    add Result Model

---
+++ Typing/Models/Result.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typing.Models
+{
+    internal class Result
+    {
+        private int _wpm;
+        public int WPM { get => _wpm; }
+        private double _accuracy;
+        public double Accuracy { get => _accuracy; }
+        private int _correctWords;
+        public int CorrectWords { get => _correctWords; }
+        private int _wrongWords;
+        public int WrongWords { get => _wrongWords; }
+
+        public void CalculateResult(bool correct)
+        {
+            _wpm++;
+            if (correct) _correctWords++; else _wrongWords++;
+            SetAccuracy();
+        }
+        public void SetAccuracy()
+        {
+            _accuracy = _correctWords / _wpm;
+        }
+    }
+}
--- Typing/ViewModels/MainVM.cs
+++ Typing/ViewModels/MainVM.cs
@@ -13,37 +13,52 @@ namespace Typing.ViewModels
     {
         private string _text;
         public string Texted { get => _text; set { _text = value; OnPropertyChanged(); CheckInputText(value); } }
-        private ObservableCollection<Text> _words = new ObservableCollection<Text>();
+        private ObservableCollection<Text> _words;
         public ObservableCollection<Text> Words { get => _words; }
         private int _k = 0;
         private DispatcherTimer _timer = null;
         private string _time;
         public string Timer { get => _time; set { _time = value; OnPropertyChanged(); } }
-        private bool _running = false;
         private int x = 60;
+        public ICommand RestartCommand { get; }
         public MainVM()
         {
-            string txt = "программисту этот паттерн позволяет менять отдельные части приложения не затрагивая другие также он может заниматься только одним компонентом вообще не представляя как работают остальные хотя для полного понимания своей работы нужно разбираться во всех аспектах написания приложений";
-            foreach (string word in txt.Split())
-                _words.Add(new Text(word));
+            Refresh();
+            _timer = new DispatcherTimer();
+            Timer = "1:00";
+            RestartCommand = new RelayCommand(obj => { Refresh(); TimerStop(); });
         }
         private void CheckInputText(string value)
         {
-            if (value.Length != 0)
+            if (value.Length != 0 && _k < Words.Count)
                 if (value[^1] == ' ')
                 {
                     Words[_k++].CheckValidWord(value.Trim());
                     Texted = "";
-                    if (!_running)
+                    if (!_timer.IsEnabled)
                     {
-                        _running = true;
                         TimerStart();
                     }
                 }
         }
+        private void Refresh()
+        {
+            _words = new ObservableCollection<Text>();
+            OnPropertyChanged("Words");
+            _k = 0;
+            Texted = "";
+            string txt = "программисту этот паттерн позволяет менять отдельные части приложения не затрагивая другие также он может заниматься только одним компонентом вообще не представляя как работают остальные хотя для полного понимания своей работы нужно разбираться во всех аспектах написания приложений";
+            foreach (string word in txt.Split())
+                _words.Add(new Text(word));
+        }
+        private void TimerStop()
+        {
+            _timer.Stop();
+            x = 60;
+            Timer = x.ToString();
+        }
         private void TimerStart()
         {
-            _timer = new DispatcherTimer();
             _timer.Tick += new EventHandler(TimerTick);
             _timer.Interval = new TimeSpan(0, 0, 1);
             _timer.Start();
@@ -55,7 +70,6 @@ namespace Typing.ViewModels
             if (x == 0)
             {
                 _timer.Stop();
-                _running = false;
             }
         }
         public event PropertyChangedEventHandler PropertyChanged;
