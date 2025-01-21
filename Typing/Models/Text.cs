8c6b3f5c6105ecd3dc23a7ca99d16ce81b3735ee
Fl1ckerxD <mihaylav.slava@yandex.ru>
2025-01-21 03:59:39 +0300

    add new Text Model

---
+++ Typing/Models/Text.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typing.Models
+{
+    internal class Text : INotifyPropertyChanged
+    {
+        private string _word;
+        public string Word { get => _word; }
+        private bool _isTrue;
+        public bool isTrue { get { return _isTrue; } set { _isTrue = value;  OnPropertyChanged(); OnPropertyChanged("Color"); } }
+        public string Color { get => isTrue ? "Green" : "Red"; }
+        public Text(string word)
+        {
+            _word = word;
+            isTrue = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Typing/ViewModels/MainVM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Typing.Classes;
+using Typing.Models;
+
+namespace Typing.ViewModels
+{
+    internal class MainVM
+    {
+        public ObservableCollection<Text> Words;
+        public MainVM()
+        {
+            string txt = "программисту этот паттерн позволяет менять отдельные части приложения не затрагивая другие также он может заниматься только одним компонентом вообще не представляя как работают остальные хотя для полного понимания своей работы нужно разбираться во всех аспектах написания приложений";
+            Words = new ObservableCollection<Text>();
+            foreach(string word in txt.Split())
+                Words.Add(new Text(word));
+        }
+    }
+}
